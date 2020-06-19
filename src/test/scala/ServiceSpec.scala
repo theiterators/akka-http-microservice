b344c315e0b816e2b3974184461ee46697cd8a13
Łukasz Sowa <contact@luksow.com>
2020-06-19 16:26:15 +0200

    Fixing scalatest

--- src/test/scala/ServiceSpec.scala
+++ src/test/scala/ServiceSpec.scala
@@ -1,13 +1,14 @@
 import akka.event.NoLogging
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.ContentTypes._
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.stream.scaladsl.Flow
-import org.scalatest._
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class ServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest with Service {
+class ServiceSpec extends AsyncFlatSpec with Matchers with ScalatestRouteTest with Service {
   override def testConfigSource = "akka.loglevel = WARNING"
   override def config = testConfig
   override val logger = NoLogging
